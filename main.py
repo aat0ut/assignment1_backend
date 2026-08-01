15225cbf40f98579f1c66b84a30c5d4e007705e1
aat0ut <amka995@uowmail.edu.au>
2026-08-01 12:50:57 +0400

    Commit 1

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from typing import TypedDict
 app = FastAPI()
 
 @app.get("/")
-def hello_world():
-    return "hello world"
+def return_details():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
